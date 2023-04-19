b556d0ac62f0552ea332854f339a1c9a0fbc6faf
attemkinen <atte.makinen1@outlook.com>
2023-04-19 13:53:45 +0300

    added react-routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import Customerlist from './components/Customerlist';
 import Traininglist from './components/Traininglist'
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+  } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Traininglist/>
+     <BrowserRouter>
+<Link to="/">Training</Link>{' '}
+<Link to="/customers">Customers</Link>{' '}
+
+<Routes>
+<Route exact path="/" element={<Traininglist/>} />
+<Route path="/customers" element={<Customerlist/>} />
+</Routes>
+</BrowserRouter>
     </div>
   );
 }
